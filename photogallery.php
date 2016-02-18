f9f2774c9801698e5ba41032fb5f4e7aaf7511c9
mvartani76 <mike.vartanian@gmail.com>
2016-02-18 16:16:17 -0500

    Added responsive stylings to the photogallery page. Updated the equal column heights javascript code so it now actually works. Sometimes the divs overlap a bit with the text and images when resizing but reloading makes all columns equal heights with no overlaps.

--- photogallery.php
+++ photogallery.php
@@ -35,28 +35,28 @@
 				</ul>
 			</nav>
 		</div>
-		<div class="row">
-			<h2>Shores of Glenwood Photo Gallery</h2>
-			<p>We are all very proud of the beauty and serenity of our neighborhood. Please click on the albums below to see the beauty of the land, the houses, the wildlife, and most importantly the people of Shores of Glenwood.</p>
+		<div class="row photogallery">
+			<h2>The Shores of Glenwood Photo Gallery</h2>
+			<p>We are all very proud of the beauty and serenity of our neighborhood. Please click on the albums below to see the beauty of the land, the houses, the wildlife, and most importantly, the people of The Shores of Glenwood.</p>
 			<p>We would love for you to share your Shores of Glenwood photo memories with us. Please contact us and we will upload your photos to this site.</p> 
 		</div>
 		<div class="row sameHeights">
-			<div class="col-1-2-4">
+			<div class="col-1-2-4" data-key="sameHeights">
 				<h3>The Nature Preserve</h3>
 			    <a href="#"><img src="images/sog-preserve-entrance-250.jpg" alt="Our Nature Preserve"></a>
 			    <p>We are so lucky to have a state nature preserve with a forest, lake and a river with lots of natural wildlife.</p>
 			</div>
-			<div class="col-1-2-4">
+			<div class="col-1-2-4" data-key="sameHeights">
 				<h3>The Front Pond</h3>
 			    <a href="#"><img src="images/sog-pond-winter-350.jpg" alt="The Front Pond"></a>
 			    <p>The pond near our subdivision entrance way is both beautiful as well as a source for winter enjoyment.</p>
 			</div>
-			<div class="col-1-2-4">
-				<h3>Shores of Glenwood at Night</h3>
-			    <a href="#"><img src="images/sog-entrance-night-250.jpg" alt="Shores of Glenwood at Night"></a>
+			<div class="col-1-2-4" data-key="sameHeights">
+				<h3>Night Time</h3>
+			    <a href="#"><img src="images/sog-entrance-night-250.jpg" alt="Night Time"></a>
 			    <p>The beauty of our neighborhood at night during the holidays...</p>
 			</div>
-			<div class="col-1-2-4">
+			<div class="col-1-2-4" data-key="sameHeights">
 				<h3>Our Houses</h3>
 			    <a href="#"><img src="images/sog-houses-300.jpg" alt="Our Houses"></a>
 			    <p>We have many beautiful houses in our neighborhood. Please see pictures of the houses that our residents have shared.</p>
