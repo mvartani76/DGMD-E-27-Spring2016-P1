7813cae0c975e2f68aff6b15e9714d0f145a15c6
mvartani76 <mike.vartanian@gmail.com>
2016-02-25 23:30:38 -0500

    Added conditional inside sameHeights function that only creates equal heights if screen size is > 479px

--- js/eqheight.js
+++ js/eqheight.js
@@ -3,17 +3,21 @@
 	        query = document.querySelectorAll(selector),
 	        elements = query.length,
 	        max = 0;
-	    if (elements) {
-	        while (elements--) {
-	            var element = query[elements];
-	            if (element.clientHeight > max) {
-	                max = element.clientHeight;
-	            }
-	        }
-	        elements = query.length;
-	        while (elements--) {
-	            var element = query[elements];
-	            element.style.height = max + 'px';
-	        }
-	    }
+
+		if ($(window).width()>479) {	    
+
+		    if (elements) {
+		        while (elements--) {
+		            var element = query[elements];
+		            if (element.clientHeight > max) {
+		                max = element.clientHeight;
+		            }
+		        }
+		        elements = query.length;
+		        while (elements--) {
+		            var element = query[elements];
+		            element.style.height = max + 'px';
+		        }
+		    }
+		}
 	}
